268e1e67e619bd7502baa146f512d590a1e169e2
cba <seba2014vpai@hotmail.com>
2020-09-01 07:07:00 -0300

    Exercise 100% finished

---
+++ Modulo3/Ejercicio1/node-gandini-sebastian/sebastian-gandini.js
@@ -0,0 +1,8 @@
+const fetch = require("node-fetch");
+fetch('https://superheroapi.com/api/10219737048390425/321/appearance')
+.then(function(response) {
+    return response.text();
+  })
+  .then(function(text){
+    console.log(text);
+  });
