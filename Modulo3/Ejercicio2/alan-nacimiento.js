3c7d0d35741a89498c774334f5c8bceb02e2510a
Alan-matea <alan21gtaonline@gmail.com>
2020-09-01 17:56:26 -0300

    Ejercicio 1 y 2

---
+++ Modulo3/Ejercicio2/alan-nacimiento.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+var server = express();
+
+server.get('/', function (req, res){
+    res.json({ nombre: 'Alan', apodo:'Alan', sobremi:'Hola soy Alan vivo en Uruguay, Montevideo.', cumple:'21 de Julio' });
+});
+
+server.get('/nombre', function (req, res){
+    res.send("Hola soy Alan!!!");
+})
+
+server.get('/apodo', function (req, res){
+    res.send("Hola soy Alan!!!");
+})
+
+server.get('/sobremi', function (req, res){
+    res.send("Hola soy Alan vivo en Uruguay, Montevideo.");
+})
+
+server.get('/cumple', function (req, res){
+    res.send("21 de Julio!!!");
+})
+
+server.post('/', function (req, res){
+    res.send('Esto es un POST');
+});
+
+server.post('/', function (req, res){
+    res.send('Esto es un PUT');
+});
+
+server.listen(3000);
