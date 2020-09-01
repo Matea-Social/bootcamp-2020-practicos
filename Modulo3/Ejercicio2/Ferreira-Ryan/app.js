2343de498bc26774610622c34e7679bc7058343a
Ryan <ryferreira1@outlook.es>
2020-09-01 17:06:42 -0300

    first-server-express

---
+++ Modulo3/Ejercicio2/Ferreira-Ryan/app.js
@@ -0,0 +1,31 @@
+svar datosJSON = require('./base-de-datos.json'); 
+const express = require('express');
+
+
+var server = express();
+
+server.get('/nombre', (req, res) => {
+    let name = datosJSON.nombre;
+    res.send('Me llamo ' + name);
+});
+server.get('/apodo', (req, res) => {
+    let apodo = datosJSON.apodo;
+    res.send('Me dicen ' + apodo);
+});
+server.get('/sobremi', (req, res) => {
+    let info = datosJSON.sobreMi;
+    res.send(info);
+});
+server.get('/cumple', (req, res) => {
+    let bth = datosJSON.cumple;
+    res.send("Cumplo el " + bth + ", así que ya saben!!!1");
+});
+server.get('/', (req, res) => {
+    let allJSON = datosJSON;
+    res.send(allJSON);
+});
+
+
+server.listen(4000);
+
+console.log("http://localhost:4000");
