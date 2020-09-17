b9464bab6fd0e3dd537c76511d2b1e8713a1d5e3
Kevin <eiikevin@gmail.com>
2020-09-17 16:17:57 -0300

    Get Post fixed + post added

--- Modulo3/Ejercicio4/iglesias-kevin/app.js
+++ Modulo3/Ejercicio4/iglesias-kevin/app.js
@@ -1,34 +1,41 @@
+/* REQUIRES */
 const mongoose = require('mongoose');
 const { func } = require('prop-types');
-
 const { Schema } =  mongoose;
 
-/* EXPRESS */
+/* EXPRESS INIT */
+
 const express = require('express');
 var server = express();
 server.use(express.json());
+server.listen(4100);
 
-//db conex
+/* MONGO DB CONNECT */
 
-mongoose.connect('mongodb+srv://kevin:asd123@cluster0.vahxu.azure.mongodb.net/canciones?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = 'mongodb+srv://kevin:123asd@cluster0.vahxu.azure.mongodb.net/canciones?retryWrites=true&w=majority'
+mongoose
+    .connect(db, { 
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+      })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log("Un error ha surgido:" + err));
 
-//definimos esquema
+
+
+/* SCHEMA */
 
 const songsSchema = new Schema({
     name: {type: String, required: true},
     album: String,
     duration: Number,
-    artist: Array,
-})
+    artist: Object,
+}, {collection: "CancionesCollections"})
 
 
-let songs = mongoose.model('Song', songsSchema);
+let songs = mongoose.model('Canciones', songsSchema);
 
-const connect = mongoose.connection;
-
-connect.once('open', function(){
-    console.log('MongoDB ha conectado!')
-});
 
 const alistSongList = async (req, res) => {
     const sFind = await songs.find()
@@ -36,5 +43,27 @@ const alistSongList = async (req, res) => {
     res.send(sFind);
 }
 
+const addSong = async (req, res) => {
+    const sBody = req.body;
+    const newSong = new Cancion(sBody);
+    newSong.save(function (err) {
+    if (err){
+        res.status(404).send("Error: No se ha podido agregar la música");
+        console.log(err);
+    } else {
+        res.status(201).send("Alerta: Se agregó la música");
+        }
+    })
+    console.log(newSong); 
+}
+
 //Get
 server.get("/", alistSongList);
+
+//Post
+
+server.post('/', addSong)
+
+
+/* CONSOLE LOGS */
+console.log("Conectado : " + server.listen);
