c5fdcbf261deb229fc6a6578dbdb8802d19fb1dd
Carolain Seija <carolainsilva1@gmail.com>
2020-09-18 17:29:20 -0300

    cambio server

--- Modulo3/Ejercicio5/seija-carolain/server/server.js
+++ Modulo3/Ejercicio5/seija-carolain/server/server.js
@@ -1,23 +1,60 @@
-                                           //EXPRESS
+//EXPRESS
 
 //importaciones de Express
 const express = require("express");
-const app = express();
+
 const app = express();
 app.use(express.json());
 
 //Importacion del controlador
 const controlador = require('./controlador');
-//MongoDB, mongoose
-const mongoose = require('mongoose');
-app.get("/", controlador.devuelveTodo);
 
+//get devuelve todo
+app.get("/", function (req, res) {
+    try {
+        let canciones = controlador.listaCanciones();
+        res.status(200);
+        res.send(canciones);
+    } catch (error) {
+        res.status(500);
+        res.send('algo salio mal');
+    }
+});
 
-//Conección mongoDB
-mongoose.connect(
-    "mongodb+srv://carolain-seija:crosiljs1@cluster0.dk3mk.mongodb.net/canciones?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  );
+//get busca una cancion
+app.get("/:name", function (req, res) {
+    try {
+        let cancion = controlador.cancionBuscada();
+        res.status(200);
+        res.send(cancion);
+    } catch (error) {
+        res.status(500);
+        res.send("algo salio mal");
+    }
+});
+//elimina a cancion
+
+app.delete("/name", function(req, res){ 
+ try { 
+     let cancionEliminar = controlador.cancionAeliminar();
+     res.status(200);
+     res.send(cancionEliminar);
+ }catch(error) { 
+     res.status(500);
+     res.send("algo salio mal");
+ }
+});
+app.post("/", function(req, res){ 
+    try { 
+        let agregaCancion = controlador.cancionAgregada();
+        res.status(200);
+        res.send(agregaCancion);
+    }catch(error) { 
+        res.status(500);
+        res.send("algo salio mal");
+    }
+   });
+
 
 //puerto del local host:
 app.listen(3000);
